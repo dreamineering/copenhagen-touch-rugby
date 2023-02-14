a5e9ac488e4c29f883ff97339cc626b11b33b350
matt <matt@dreamineering.com>
2023-02-14 22:42:08 +0100

    margin on map

--- sections/Location.jsx
+++ sections/Location.jsx
@@ -5,21 +5,21 @@ import styles from "../styles";
 const Location = () => {
   return (
     <section className={`${styles.paddings} bg-green-900`} id="Location">
-      <div className="mx-auto lg:ml-8 xl:ml-36 lg:w-2/3">
-        <div className="">
+      <div className="mx-auto">
+        <div className="md:mx-10 xl:mx-24">
           <h1 className="text-4xl pb-6 tracking-tight font-bold text-gray-300 sm:text-5xl md:text-5xl xl:text-6xl">
             Find us at <span className="text-white ">Faelledparken</span>
           </h1>
-        </div>
 
-        <div className="w-full ">
-          <Image
-            className="object-cover object-center w-full"
-            alt="Where we play"
-            src={"/img/map.png"}
-            width={600}
-            height={600}
-          />
+          <div className="w-full ">
+            <Image
+              className="object-cover object-center w-full"
+              alt="Where we play"
+              src={"/img/map.png"}
+              width={600}
+              height={600}
+            />
+          </div>
         </div>
       </div>
     </section>
