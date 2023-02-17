589f0e72567cca660eba5542b08669ebfbd8d9fb
matt <matt@dreamineering.com>
2023-02-17 13:10:00 +0100

    update

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -27,8 +27,8 @@ export function Footer() {
                 <NavLink href="/uses">Uses</NavLink>
               </div>
               <p className="text-sm text-zinc-400 dark:text-zinc-500">
-                &copy; {new Date().getFullYear()} Spencer Sharp. All rights
-                reserved.
+                &copy; {new Date().getFullYear()} Copenhagen Touch Rugby. All
+                rights reserved.
               </p>
             </div>
           </Container.Inner>
