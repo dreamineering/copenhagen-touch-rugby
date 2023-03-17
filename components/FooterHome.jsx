647d04dfd05c873bf52fd139187d76a4663384ec
wik <matt@dreamineering.com>
2023-03-17 14:08:59 +0100

    update stuff

--- components/FooterHome.jsx
+++ components/FooterHome.jsx
@@ -11,14 +11,14 @@ const Footer = () => (
   <footer className="px-10 pt-20 pb-10 relative bg-black">
     <div className="footer-gradient" />
 
-    <div className={`${styles.innerWidth} mx-auto flex flex-col gap-8 `}>
+    <div className={`${styles.innerWidth} mx-auto flex flex-col gap-2`}>
       <div className="flex items-center justify-between flex-wrap gap-5">
-        <h4 className="font-bold md:text-[32px] text-[18px] uppercase text-white opacity-70">
+        <h4 className="font-bold text-[18px] md:text-[24px] uppercase text-white opacity-70">
           Copenhagen Touch rugby
         </h4>
       </div>
-      <h4 className="text-[18px] text-white opacity-70">Sponsored by</h4>
-      <div className="w-[260px]">
+      <h4 className="text-[14px] md:text-[16px] text-white opacity-70">Sponsored by</h4>
+      <div className="w-[260px] md:w-[400px]">
         <Link href="https://www.bootleggers.dk/">
           <Image
             src={'/img/bootleggers-logo.webp'}
@@ -55,7 +55,7 @@ const Footer = () => (
             ))}
           </div>
         </div>
-        <p className="text-white opacity-70 pt-6 text-lg md:text-xl text-center">
+        <p className="text-white opacity-70 pt-6 text-lg md:text-4xl text-center">
           <Link href={'https://www.facebook.com/groups/172852439436767/events'}>
             <span className="hover:text-green-400">Check play is going ahead</span>
           </Link>
