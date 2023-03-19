c3c8fb2ceab6de6b257bc426ccb8746665bdb6db
matt <matt@dreamineering.com>
2023-03-19 15:45:08 +0100

    add pixel following nextjs example

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,4 +1,8 @@
 import { useEffect, useRef } from "react";
+import Script from "next/script";
+import { useRouter } from "next/router";
+import * as fbq from "../lib/fpixel";
+
 import { Footer, Navbar } from "@/components";
 
 import "@/styles/globals.css";
@@ -17,21 +21,41 @@ function usePrevious(value) {
 export default function App({ Component, pageProps, router }) {
   let previousPathname = usePrevious(router.pathname);
 
-  useEffect(() => {
-    import("react-facebook-pixel")
-      .then((x) => x.default)
-      .then((ReactPixel) => {
-        ReactPixel.init("2237940296411518");
-        ReactPixel.pageView();
+  // const router = useRouter();
 
-        router.events.on("routeChangeComplete", () => {
-          ReactPixel.pageView();
-        });
-      });
+  useEffect(() => {
+    // This pageview only triggers the first time (it's important for Pixel to have real information)
+    fbq.pageview();
+
+    const handleRouteChange = () => {
+      fbq.pageview();
+    };
+
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
   }, [router.events]);
 
   return (
     <>
+      <Script
+        id="fb-pixel"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', ${fbq.FB_PIXEL_ID});
+          `,
+        }}
+      />
       <div className="fixed inset-0 flex justify-center sm:px-8">
         <div className="flex w-full max-w-7xl lg:px-8">
           <div className="w-full bg-white ring-1 ring-zinc-100" />
