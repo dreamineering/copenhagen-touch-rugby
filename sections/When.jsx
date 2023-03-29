a40c9b6ab96841b30f51f6ee0a5056bce92ffb19
wik <matt@dreamineering.com>
2023-03-29 22:21:05 +0200

    update

--- sections/When.jsx
+++ sections/When.jsx
@@ -21,19 +21,8 @@ const When = () => {
 
   return (
     <section className={`xs:p-8 bg-green-800 px-6 py-8 sm:p-8`} id="when">
-      <div className="pt-10 md:mx-10 xl:mx-60 xl:pt-24 2xl:mx-80">
-        <h2 className="pt-4 text-center text-3xl uppercase tracking-tight text-white sm:text-4xl md:text-4xl xl:text-5xl">
-          New Players
-          <br />
-          <Link className="" href={'/about'}>
-            <span className="pb-4 text-6xl font-bold tracking-tight text-white opacity-90 hover:text-red-600  sm:text-8xl md:text-8xl xl:text-9xl">
-              welcome
-            </span>
-          </Link>
-        </h2>
-      </div>
-      <div className="mx-auto max-w-7xl px-4 pt-8 pb-16 sm:py-16 sm:px-6 lg:px-8 lg:pt-20 lg:pb-40">
-        <dl className="mt-10 text-center sm:mx-auto sm:grid sm:max-w-3xl sm:grid-cols-2 sm:gap-8">
+      <div className="mx-auto px-4 pb-16 sm:py-16 sm:px-6 lg:px-8 lg:pt-20 lg:pb-40">
+        <div className="pt-10 text-center sm:mx-auto">
           <div className="mt-10 flex flex-col sm:mt-0 ">
             <Link className="" href={'/play'}>
               <h1 className="md:text-10xl xl:text-12xl text-8xl font-bold uppercase tracking-tight text-white opacity-90 hover:text-red-600 sm:text-9xl">
@@ -41,14 +30,17 @@ const When = () => {
               </h1>
             </Link>
           </div>
-          {summertime}
-          <div className="mt-10 flex flex-col sm:mt-0">
-            <dt className="order-2 mt-2 text-4xl font-medium leading-6 text-white md:text-6xl">
-              SUN
-            </dt>
-            <dd className="order-1 text-8xl font-extrabold text-white md:text-8xl">10:30</dd>
+          <div className="pt-12 flex flex-col md:flex-row mx-auto text-center w-4/5 space-y-10" >
+            <div className="flex flex-col mx-auto space-y-4">
+              <div className="text-5xl font-medium leading-6 text-white md:text-7xl">SUN</div>
+              <div className="text-8xl font-extrabold text-white md:text-8xl">10:30</div>
+            </div>
+            <div className="flex flex-col mx-auto space-y-4">
+              <div className="text-5xl font-medium leading-6 text-white md:text-7xl">WED</div>
+              <div className="text-8xl font-extrabold text-white md:text-8xl">18:00</div>
+            </div>
           </div>
-        </dl>
+        </div>
       </div>
     </section>
   );
